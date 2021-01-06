b9f6f40d93c5a8c05805f701b5760e5fc6eed5ec
Maxence Raballand <47566657+maxencerb@users.noreply.github.com>
2021-01-06 20:52:05 +0100

    init commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig : {
+    apiKey: "AIzaSyDIea_H2rsHMd885vMPYBDGOjh1MRkRHSU",
+    authDomain: "picture-of-2020.firebaseapp.com",
+    projectId: "picture-of-2020",
+    storageBucket: "picture-of-2020.appspot.com",
+    messagingSenderId: "217024754903",
+    appId: "1:217024754903:web:03c5966ac15e9fae936c92",
+    measurementId: "G-BKHDNCXYTQ"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig : {
+    apiKey: "AIzaSyDIea_H2rsHMd885vMPYBDGOjh1MRkRHSU",
+    authDomain: "picture-of-2020.firebaseapp.com",
+    projectId: "picture-of-2020",
+    storageBucket: "picture-of-2020.appspot.com",
+    messagingSenderId: "217024754903",
+    appId: "1:217024754903:web:03c5966ac15e9fae936c92",
+    measurementId: "G-BKHDNCXYTQ"
+  }
 };
 
 /*
